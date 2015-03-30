0d17d96e5ffbed00f051c15f635df92ffe00b37a
Aritz <Aritz@Cortain>
2015-03-30 10:50:20 -0400

    All systems are a go!

---
+++ ResistTech/src/resistMain/ResistInterface.java
@@ -0,0 +1,11 @@
+package resistMain;
+
+import javax.swing.JOptionPane;
+
+public class ResistInterface {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, world!\r\n");
+		JOptionPane.showMessageDialog(null, "Hello, world!");
+	}
+}
